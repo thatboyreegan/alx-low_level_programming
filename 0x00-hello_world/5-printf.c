18bf0540f6fbed6b68179111a9d3745485c705ec
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-08 11:29:57 -0800

    5-printf.c 2.0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is
- * 		a piece of art, followed by a new line
+ * main - prints a string
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper prammar, but the outcome is a piece of art,\n");
 
 	return (0);
-}	
+}
