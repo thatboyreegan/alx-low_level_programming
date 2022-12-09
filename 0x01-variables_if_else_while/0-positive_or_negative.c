44811efb6d0e6b128ec9a384677e97e047a97d86
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-09 02:39:24 -0800

    0-positive_or_negative.c 2.0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Prints a random number and states
- * 	whether a number is positive or negative.
+ * whether a number is positive or negative.
  *
  * Return: Always 0.
  */
