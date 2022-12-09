ee2f377bdcfeadaf280012fae0495221b80a5f93
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-09 04:31:37 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if(alphabet != 'e' && alphabet != 'q')
+		if (alphabet != 'e' && alphabet != 'q')
 			putchar(alphabet);
 	}
 	putchar('\n');
