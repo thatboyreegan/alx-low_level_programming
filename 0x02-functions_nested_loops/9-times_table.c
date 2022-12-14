310c590cf7ef184fc89c034458ce1f0f00a44fb2
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-14 06:34:28 -0800

    9-times_tables.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
  * times_table - prints tables.
- *
  * Return: Always 0.
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int a,b,c;
 
 	for (a = 0; a <= 9; a++)
 	{
