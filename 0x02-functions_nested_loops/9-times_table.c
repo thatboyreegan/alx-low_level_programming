1629ee0b3a5d68e434f51ace294d61d00ac0d93c
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-14 06:38:47 -0800

    9-times_table.c 2.0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int a,b,c;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
@@ -24,7 +24,7 @@ void times_table(void)
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
-				if ( b < 9)
+				if (b < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
