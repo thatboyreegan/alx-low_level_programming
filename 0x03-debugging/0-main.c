9f5515b7bf889a9417f8a7957b23a0126254e01d
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-15 14:10:25 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests functions and prints if negative or positive.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0-main.c is a a program that the function positive_or_negative() gives the correct output when given a case of 0.
