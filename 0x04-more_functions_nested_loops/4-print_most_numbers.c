2f7b4bbdcba3846a3109b4d5bf24784b753391e0
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-16 00:56:30 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if ((i == 2) || (i == 4))
-		{
-			continue;
-		}
-		_putchar(i);
+		if (i != 50 && i != 52)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
