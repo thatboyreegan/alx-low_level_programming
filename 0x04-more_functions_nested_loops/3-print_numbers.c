6844ed2e379f4d29da1236521942880f00bc2cb9
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-16 00:47:22 -0800

    Again

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print-numbers - prints numbers from 0 to 9
+ * print_numbers - prints numbers from 0 to 9
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 			_putchar(i);
 	}
