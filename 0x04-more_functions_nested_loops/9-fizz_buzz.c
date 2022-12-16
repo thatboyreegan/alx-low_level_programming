1e6ec5c562833f8cd3f8ea9a48d1689d64289734
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-16 02:02:34 -0800

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 		if (i < 100)
 		{
 			if (i % 3 == 0 && i % 5 == 0)
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			else if (i % 3 == 0)
-				printf("Fizz ");
+				printf("Fizz");
 			else if (i % 5 == 0)
-				printf("Buzz ");
+				printf("Buzz");
 			else 
 				printf("%d", i);
 		}
