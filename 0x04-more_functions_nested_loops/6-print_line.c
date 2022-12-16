fac00d8009f42a13f5c4ffcdf5ef9594f0803d4a
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-16 01:06:41 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,10 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+			_putchar('_');
 	}
-	else
-	{
-		for (i = 1; i <= n; i++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
