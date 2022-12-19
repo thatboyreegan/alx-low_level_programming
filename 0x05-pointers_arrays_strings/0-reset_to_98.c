c068b53e6389cbcf49bc6a2f7c8da76a178afdc9
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2022-12-19 07:38:42 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value the pointer points to.
+ * @n: pointer.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c is a progeram that takes a pointer to an int as parameter and updates the value it points to to 98.
