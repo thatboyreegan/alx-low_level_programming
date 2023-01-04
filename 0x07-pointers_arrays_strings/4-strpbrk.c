868798493052c4659b56894cdfd5de487f16a574
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-01-04 04:01:09 +0300

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any set of bytes.
+ * @s: first string.
+ * @accept: second string
+ * Return: pointer to byte in s matching bytes in accept.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + 1) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
