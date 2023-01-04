0252f7fbef24ba68acba1dadb0851d504a9dbd42
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-01-04 04:46:03 +0300

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of pointer to char.
+ * @s: pointer to pinter.
+ * @to: string to set.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
