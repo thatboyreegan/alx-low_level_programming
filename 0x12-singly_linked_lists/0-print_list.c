baba9e8b72b5b023db125d7f1ddd5c5f3777fed7
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-01-31 06:50:29 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@ size_t print_list(const list_t *h)
 	size_t num;
 
 	num = 0;
-	while (h != NULL)
+	if (!h)
+		return (num);
+
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[0] (nil)\n");
+
 		num++;
+		h = h->next;
 	}
 	return (num);
 }
